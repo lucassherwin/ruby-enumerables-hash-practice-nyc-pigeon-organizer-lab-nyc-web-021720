ec8258fd1d011e5193eb0a5c58149fac124d56b3
Lucas Sherwin <lksherwin@gmail.com>
2020-01-30 19:52:14 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  organized_pigeons = {}
+  data.each do |key, value|
+    value.each do |new_val, pigeon_names|
+      pigeon_names.each do |name|
+        if !organized_pigeons[name]
+          organized_pigeons[name] = {}
+        end
+        if !organized_pigeons[name][key]
+          organized_pigeons[name][key] = []
+        end
+        organized_pigeons[name][key] << new_val.to_s
+      end
+    end
+  end
+  organized_pigeons
 end
